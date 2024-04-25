c3e08fee0394839768db3f3aa7c224534f298922
Eric Yao <elyao@dons.usfca.edu>
2024-04-25 11:14:19 -0700

    required file for linked view

---
+++ myviz/eventEmitter.js
@@ -0,0 +1,16 @@
+const eventEmitter = {
+    events: {},
+    on: function(event, listener) {
+      if (!this.events[event]) {
+        this.events[event] = [];
+      }
+      this.events[event].push(listener);
+    },
+    emit: function(event, data) {
+      if (this.events[event]) {
+        this.events[event].forEach(listener => listener(data));
+      }
+    }
+  };
+  
+  export default eventEmitter;
